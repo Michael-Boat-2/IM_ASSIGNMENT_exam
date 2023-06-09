89b59e20d17a0dab266fbcc7fe74b5a313585bf7
Michael-Boateng <131008187+Michael-Boat-2@users.noreply.github.com>
2023-06-09 14:26:57 +0200

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,14 +1,11 @@
 
     // Scroll to top function 
     function ScrollToTop(){
-         
-        alert("Scroll to top");
-
-          window.scrollTo({
-            top: 0,
-            behavior: 'smooth',
-            duration: 3000 
-          });
+        window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+        duration: 3000 
+        });
     }
    
 
